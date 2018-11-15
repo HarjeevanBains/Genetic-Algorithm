51e7bb00d6cb12952fc6a04756c6013a5a787b3d
Jeevan Bains <harjeevanbains@gmail.com>
2018-11-14 18:53:08 -0800

    now able to randomize a list of cities

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <algorithm>
 #include "tour.hpp"
 
 double generateCoordinate() {
@@ -8,11 +9,11 @@ double generateCoordinate() {
     return distribution(generator);
 }
 
-list<city> makeCityList(int size){
+list<city> makeCityList(int size) {
     char name = 'A';
     list<city> mix;
-    for(int i =0; i<size;++i){
-        mix.push_back({name++,generateCoordinate(),generateCoordinate()});
+    for (int i = 0; i < size; ++i) {
+        mix.push_back({name++, generateCoordinate(), generateCoordinate()});
     }
     return mix;
 }
@@ -24,12 +25,24 @@ void printCities(list<city> &cities) {
     }
 }
 
+list<city> mixCities(list<city> cities) {
+    list<city> mixed;
+    vector<city> copyVector{std::begin(cities), std::end(cities)};;
+    random_shuffle(copyVector.begin(), copyVector.end());
+    copy(copyVector.begin(), copyVector.end(), std::back_inserter(mixed));
+    return mixed;
+}
+
 using namespace std;
+
 int main() {
-    list<city> mix = makeCityList(10);
-   // city a{name++,generateCoordinate(),generateCoordinate()};
+    list<city> list1 = makeCityList(10);
+    // city a{name++,generateCoordinate(),generateCoordinate()};
     //city b{name++,generateCoordinate(),generateCoordinate()};
-    cout << "City info:" << "\n"<< endl;
-    printCities(mix);
+    cout << "City info:" << "\n" << endl;
+    printCities(list1);
+    cout << "----" << endl;
+    list<city> mix1 = mixCities(list1);
+    printCities(mix1);
     return 0;
 }
